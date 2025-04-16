7c0dde3c3bf4ffaa5846f3882ef73001d9e36eea
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-04-17 00:22:31 +0530

    Initial Commit

---
+++ Skeleton/SkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkeletonApp.swift
+//  Skeleton
+//
+//  Created by Saurabh Jaiswal on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
